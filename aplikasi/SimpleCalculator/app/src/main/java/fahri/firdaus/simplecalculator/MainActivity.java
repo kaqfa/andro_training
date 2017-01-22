148dc622c126609e4493f34dd2ce06c94b631f2e
kaqfa <elfaatta@gmail.com>
2017-01-22 23:31:29 +0700

    Simple calculator finish

--- aplikasi/SimpleCalculator/app/src/main/java/fahri/firdaus/simplecalculator/MainActivity.java
+++ aplikasi/SimpleCalculator/app/src/main/java/fahri/firdaus/simplecalculator/MainActivity.java
@@ -2,7 +2,10 @@ package fahri.firdaus.simplecalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,8 +15,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void calculate(){
+    public void calculate(View view){
+        TextView txtResult = (TextView) findViewById(R.id.result);
         EditText edit1 = (EditText) findViewById(R.id.num1);
         EditText edit2 = (EditText) findViewById(R.id.num2);
+        double num1 = Double.parseDouble(edit1.getText().toString());
+        double num2 = Double.parseDouble(edit2.getText().toString());
+
+        RadioGroup operator = (RadioGroup) findViewById(R.id.operator);
+        int selected = operator.getCheckedRadioButtonId();
+
+        if(selected == R.id.plus){
+            txtResult.setText(Double.toString(num1+num2));
+        } else if(selected == R.id.minus){
+            txtResult.setText(Double.toString(num1-num2));
+        } else if(selected == R.id.multiply){
+            txtResult.setText(Double.toString(num1*num2));
+        } else {
+            txtResult.setText(Double.toString(num1/num2));
+        }
     }
 }
