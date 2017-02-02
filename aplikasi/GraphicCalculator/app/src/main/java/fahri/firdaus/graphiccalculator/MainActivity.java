dc379319e132cf5fb41d591186cfcdc68a01acd3
fahri firdausillah <elfaatta@gmail.com>
2017-02-02 14:47:41 +0700

    graphic calculator nested linear

---
+++ aplikasi/GraphicCalculator/app/src/main/java/fahri/firdaus/graphiccalculator/MainActivity.java
@@ -0,0 +1,88 @@
+package fahri.firdaus.graphiccalculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText monitor;
+    private float result;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        monitor = (EditText) findViewById(R.id.monitor);
+    }
+
+    public void addNum(){
+
+    }
+
+    public void cZero(View view){
+
+    }
+
+    public void cOne(View view){
+
+    }
+
+    public void cTwo(View view){
+
+    }
+
+    public void cThree(View view){
+
+    }
+
+    public void cFour(View view){
+
+    }
+
+    public void cFive(View view){
+
+    }
+
+    public void cSix(View view){
+
+    }
+
+    public void cSeven(View view){
+
+    }
+
+    public void cEight(View view){
+
+    }
+
+    public void cNine(View view){
+
+    }
+
+    public void cPlus(View view){
+
+    }
+
+    public void cMin(View view){
+
+    }
+
+    public void cMul(View view){
+
+    }
+
+    public void cDiv(View view){
+
+    }
+
+    public void cComma(View view){
+
+    }
+
+    public void cCi(View view){
+
+    }
+}
